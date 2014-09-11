c8dc5333caedd96e328a09efceb662934fbbfff8
Sahil <sahildua2305@gmail.com>
2014-09-12 03:15:14 +0530

    Added support for returning all the redirect URLs

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import httplib
 import urlparse
 
-def unshorten_me(url):
+def expandURL(url):
+    urls = [url]
+    return unshorten_me(url, urls)
+
+def unshorten_me(url, url_list):
     parsed = urlparse.urlparse(url)
     h = httplib.HTTPConnection(parsed.netloc)
     h.request('HEAD', parsed.path)
     response = h.getresponse()
     if response.status/100==3 and response.getheader('location'):
-        return unshorten_me(response.getheader('location'))
+        url_list.append(response.getheader('location'))
+        return unshorten_me(response.getheader('location'), url_list)
     else:
-        return url
+        if url not in url_list:
+            url_list.append(url)
+        return url_list
 
 #print unshorten_me('http://bit.ly/1whCayD')
-print unshorten_me('http://tuq.in/KxEp')
+#print unshorten_me('http://tuq.in/KxEp')
+print expandURL('http://tuq.in/KxEp')
