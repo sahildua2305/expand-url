ca99d9eeefa7a2902b016be36052968723605db5
Sahil <sahildua2305@gmail.com>
2014-09-22 21:14:14 +0530

    Update Bottle App

--- bottle/response-test1.py
+++ bottle/response-test1.py
@@ -1,4 +1,4 @@
 import requests
 
-r = requests.get('http://localhost:8080/expand-url', params={'url':'http://tuq.in/myspeed'})
-print r.content
+r = requests.get('http://sahildua2305.pythonanywhere.com/expand', params={'url':'http://tuq.in/myspeed'})
+print r
